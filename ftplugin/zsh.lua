68cf92fad000fb1ac307351be0d69398d13b2eb1
Sam Gabel <sam@gabelcloud.com>
2026-06-25 20:07:40 -0700

    fix(ft): append invis tab to listchars instead of replace
    Base listchars is in /lua/config/options.lua

--- ftplugin/zsh.lua
+++ ftplugin/zsh.lua
@@ -1,3 +1,3 @@
 vim.opt_local.expandtab = false -- convert tabs to spaces
 vim.opt_local.tabstop = 4 -- tab length is 4 characters long
-vim.opt_local.listchars = { tab = "⠀⠀" } -- formats the literal tab character to be invisible
+vim.opt_local.listchars:append({ tab = "⠀⠀" }) -- make literal tabs invisible, keep inherited trail marker
