6ef58ad751d13bb069750bff950ead94da8ec5d2
Sam Gabel <sam@gabelcloud.com>
2026-07-27 16:13:13 -0700

    chore: bash ftplugin

---
+++ ftplugin/bash.lua
@@ -0,0 +1,3 @@
+vim.opt_local.expandtab = false -- convert tabs to spaces
+vim.opt_local.tabstop = 4 -- tab length is 4 characters long
+vim.opt_local.listchars:append({ tab = "⠀⠀" }) -- make literal tabs invisible, keep inherited trail marker
